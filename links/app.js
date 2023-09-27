5fcbc12e07091c37699a8b59405a61f5b102d088
iOssme <131443630+iOssme@users.noreply.github.com>
2023-09-27 15:11:27 +0300

    Add files via upload

---
+++ links/app.js
@@ -0,0 +1,37 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+async function copyText(e) {
+//prevent button going to the site
+     e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try{
+        await navigator.clipboard.writeText(link)
+        alert("copied the text: " + link)
+    } catch (err){
+        console.error(err)
+    }
+}
+shareButtons.forEach(shareButton => 
+    shareButton.addEventListener('click', copyText))
+/*share-button*/
+    const shareButton = document.querySelectorAll('.share-button')
+console.log(shareButton)
+
+async function copyText(e) {
+//prevent button going to the site
+     e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try{
+        await navigator.clipboard.writeText(link)
+        alert("copied the text: " + link)
+    } catch (err){
+        console.error(err)
+    }
+}
+shareButton.forEach(shareButton => 
+    shareButton.addEventListener('click', copyText))
+
+
